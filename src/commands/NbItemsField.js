3bdeceff502acb9e0878681bef43fd293bc8d60b
Francois Zaninotto <fzaninotto@gmail.com>
2016-12-08 15:52:01 +0100

    Using new FunctionField

--- src/commands/NbItemsField.js
+++ src/commands/NbItemsField.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import { FunctionField } from 'admin-on-rest/lib/mui'
 
-const NbItemsField = ({ record }) => <span>{record && record.basket.length}</span>;
+const NbItemsField = (props) => <FunctionField {...props} render={record => record.basket.length} />;
 
 NbItemsField.defaultProps = {
     label: 'Nb Items',
