6660c273dae9c7efbeb4aeccb71b712eed3ad2c6
Francois Zaninotto <fzaninotto@gmail.com>
2017-02-21 16:05:42 +0100

    Add link to pending reviews from dashboard

--- src/dashboard/PendingReviews.js
+++ src/dashboard/PendingReviews.js
@@ -4,18 +4,22 @@ import { List, ListItem } from 'material-ui/List';
 import CommentIcon from 'material-ui/svg-icons/communication/comment';
 import Avatar from 'material-ui/Avatar';
 import { translate } from 'admin-on-rest';
+import { Link } from 'react-router';
 
 import StarRatingField from '../reviews/StarRatingField';
 
 const styles = {
+    titleLink: { textDecoration: 'none', color: '#000' },
     card: { borderLeft: 'solid 4px #f44336', flex: 1, marginRight: '1em' },
     icon: { float: 'right', width: 64, height: 64, padding: 16, color: '#f44336' },
 };
 
+const query = encodeURIComponent(JSON.stringify({ status: 'pending' }));
+
 export default translate(({ reviews = [], customers = {}, nb, translate }) => (
     <Card style={styles.card}>
         <CommentIcon style={styles.icon} />
-        <CardTitle title={nb} subtitle={translate('pos.dashboard.pending_reviews')} />
+        <CardTitle title={<Link to={`/reviews?filter=${query}`} style={styles.titleLink}>{nb}</Link>} subtitle={translate('pos.dashboard.pending_reviews')} />
         <List>
             {reviews.map(record =>
                 <ListItem
