68624cd4b81df277a137e66ef9c7dc0149894bd2
Francois Zaninotto <fzaninotto@gmail.com>
2017-02-21 16:05:58 +0100

    Make stars rating smaller

--- src/reviews/StarRatingField.js
+++ src/reviews/StarRatingField.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Icon from 'material-ui/svg-icons/action/stars';
 
+const style = { opacity: 0.87, width: 20, height: 20 };
+
 const StarRatingField = ({ record }) => (
     <span>
-        {Array(record.rating).fill(true).map((_, i) => <Icon key={i} style={{ opacity: 0.87 }} />)}
+        {Array(record.rating).fill(true).map((_, i) => <Icon key={i} style={style} />)}
     </span>
 );
+
 StarRatingField.defaultProps = {
     label: 'resources.reviews.fields.rating',
     source: 'rating',
