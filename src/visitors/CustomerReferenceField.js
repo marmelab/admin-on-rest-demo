6d6de380ae87ca48bce6b580617f9e2819ee4793
Nikita <nikita.arykov@gmail.com>
2017-03-01 10:07:02 +0200

    add missing semicolon

--- src/visitors/CustomerReferenceField.js
+++ src/visitors/CustomerReferenceField.js
@@ -6,7 +6,7 @@ const CustomerReferenceField = (props) => (
     <ReferenceField source="customer_id" reference="customers" {...props}>
         <FullNameField />
     </ReferenceField>
-)
+);
 CustomerReferenceField.defaultProps = {
     source: 'customer_id',
     addLabel: true,
