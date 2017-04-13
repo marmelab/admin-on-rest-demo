e2e83acf6a81718b4c19e2edb87dafdc210ee999
Francois Zaninotto <fzaninotto@gmail.com>
2017-04-13 18:21:05 +0200

    Move PropTypes to external package for React upgrade

--- src/buttons/EditButton.js
+++ src/buttons/EditButton.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import IconButton from 'material-ui/IconButton';
 import {cyan500} from 'material-ui/styles/colors';
