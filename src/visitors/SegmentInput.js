9a50a8035746f9767a7a9530837f9791cde00796
Francois Zaninotto <fzaninotto@gmail.com>
2017-06-28 23:34:39 +0200

    Rename inner const

--- src/visitors/SegmentInput.js
+++ src/visitors/SegmentInput.js
@@ -3,16 +3,16 @@ import { translate, SelectInput } from 'admin-on-rest';
 
 import segments from '../segments/data';
 
-const SegmentsInput = ({ translate, ...rest }) => (
+const SegmentInput = ({ translate, ...rest }) => (
     <SelectInput {...rest} choices={segments.map(segment => ({ id: segment.id, name: translate(segment.name) }))} />
 );
 
-const TranslatedSegmentsInput = translate(SegmentsInput);
+const TranslatedSegmentInput = translate(SegmentInput);
 
-TranslatedSegmentsInput.defaultProps = {
+TranslatedSegmentInput.defaultProps = {
     addLabel: true,
     addField: true,
     source: 'groups',
 };
 
-export default TranslatedSegmentsInput;
+export default TranslatedSegmentInput;
